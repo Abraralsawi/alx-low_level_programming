092f989901e03badf04f3718e4d7d1f6623e9579
Abraralsawi <abraralsawi310@gmail.com>
2023-08-24 12:12:43 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *)
+{
+	int i;
+	i = 0;
+	while (n[i] != '\0')
+	{
+	if (n[i] >= 'a' && n[i] <='z')
+	n[i] = n[i] - 32;
+	i++;
+	}
+	return (n);
+}
+	
+		
